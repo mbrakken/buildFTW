b337941e91f696c4c5e1142d5690efb4a50f35ec
Marc Brakken <marc.brakken@gmail.com>
2013-10-16 22:20:30 -0500

    module updates - rpx, context, ajax_comments

--- sites/all/themes/theme_the_world/template.php
+++ sites/all/themes/theme_the_world/template.php
@@ -5,34 +5,34 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
 
 /**
-* Theme override for theme_menu_item()
-
-function theme_the_world_links($variables) {
-  $links = $variables['og_node_create_links'];
-  
-  
-  if (!empty($extra_class)) {
-    $class .= ' '. $extra_class;
-  }
-
-  // Add unique identifier
-  static $item_id = 0;
-  $item_id += 1;
-  $id .= ' ' . 'link-item-custom-id-' . $item_id;
-  // Add semi-unique class
-  $class .= ' ' . preg_replace("/[^a-zA-Z0-9]/", "", strip_tags($link));
-
-  return '<li class="'. $class .'" id="' . $id . '">'. $link ."</li>\n";
-}
-
-*/
+ * Theme override for theme_menu_item()
+ *
+ * function theme_the_world_links($variables) {
+ *   $links = $variables['og_node_create_links'];
+ *
+ *
+ *  if (!empty($extra_class)) {
+ *    $class .= ' '. $extra_class;
+ *  }
+ *
+ *  // Add unique identifier
+ *  static $item_id = 0;
+ *  $item_id += 1;
+ *  $id .= ' ' . 'link-item-custom-id-' . $item_id;
+ *  // Add semi-unique class
+ *  $class .= ' ' . preg_replace("/[^a-zA-Z0-9]/", "", strip_tags($link));
+ *
+ *  return '<li class="'. $class .'" id="' . $id . '">'. $link ."</li>\n";
+ * }
+ *
+ */
 ?>
 <?php
 
@@ -50,7 +50,7 @@ function theme_the_world_item_list($vars) {
   if (!empty($items)) {
     $output .= "<$type" . drupal_attributes($attributes) . '>';
     $num_items = count($items);
-    
+
     foreach ($items as $i => $item) {
       $attributes = array();
       $children = array();
@@ -70,29 +70,29 @@ function theme_the_world_item_list($vars) {
       else {
         $data = $item;
       }
-      
+
       if (count($children) > 0) {
         // Render nested list.
         $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
       }
-      
+
       if ($i == 0) {
         $attributes['class'][] = 'donkey';
       }
-      
+
       if ($i == $num_items - 1) {
         $attributes['class'][] = 'banana';
       }
-     
-      
+
+
       $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>";
     }
-    
+
     $output .= "</$type>";
   }
-  
+
   $output .= '</div>';
-  
+
   return $output;
 }
 ?>
