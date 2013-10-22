bc33142ef2a1b920dc0ec8050f94e0a2143bb718
Marc Brakken <marc.brakken@gmail.com>
2013-10-21 22:17:02 -0500

    added fonts, adjusted styles, new panels layout for projects

---
+++ sites/all/themes/theme_the_world/layouts/buildftw_2_10/buildftw-2-10.tpl.php
@@ -0,0 +1,14 @@
+<div class="panel-display omega-grid buildftw-2-10" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['top']; ?></div>
+  </div>
+  <div class="panel-panel grid-2 alpha">
+    <div class="inside"><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel grid-10 omega">
+    <div class="inside"><?php print $content['right']; ?></div>
+  </div>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['bottom']; ?></div>
+  </div>
+</div>
