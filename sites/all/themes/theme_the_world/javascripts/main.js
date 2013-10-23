91defc8edfa331b489f96b0b96a387cc25996484
Marc Brakken <marc.brakken@gmail.com>
2013-10-22 21:55:43 -0500

    restyling /projects page, updating masonry to dev, bringing in some js

---
+++ sites/all/themes/theme_the_world/javascripts/main.js
@@ -0,0 +1,38 @@
+(function ($) {$(document).ready(function() {
+  if ($('body').hasClass('context-projects')) {
+    $(function() {
+      if ($(window).width() > 720) {
+        var headfix = $('.views-exposed-widgets');
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > (200) && $(headfix.css('position') == 'static')) {
+            headfix.addClass('fixed');
+            $('.grid-9.region.region-content').addClass('fixed');
+          } else if ($(this).scrollTop() <= (200) && headfix.hasClass('fixed')) {
+            headfix.removeClass('fixed');
+            $('.grid-9.region.region-content').removeClass('fixed');
+          };
+        });
+      };
+    });
+  }
+});})(jQuery);
+
+
+// if ($('body').hasClass('purchases') && $('body').hasClass('new')) {
+//     $(function() {
+//       if ($(window).width() < 600) {
+//         var headfix = $('body.purchases.new header, body.purchases.new .purchase-wrapper'), pos = headfix.offset();
+//         $(window).scrollTop(85);
+//         $(window).scroll(function() {
+//           if ($(this).scrollTop() > (pos.top + 10) && $(headfix.css('position') == 'static')) {
+//             headfix.addClass('fixed');
+//             var headheight = $('header').height();
+//             $('.purchase-wrapper').css('padding-top', (headheight));
+//           } else if ($(this).scrollTop() <= (pos.top + 10) && headfix.hasClass('fixed')) {
+//             headfix.removeClass('fixed');
+//             $('.purchase-wrapper').css('padding-top', '0');
+//           };
+//         });
+//       };
+//     });
+//   };
