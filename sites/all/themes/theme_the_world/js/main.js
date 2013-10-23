95b29ac60602ce3d8dc650ad1c7576a616256bf0
Marc Brakken <marc.brakken@gmail.com>
2013-10-23 09:21:15 -0500

    added js to filters on resources page

--- sites/all/themes/theme_the_world/js/main.js
+++ sites/all/themes/theme_the_world/js/main.js
@@ -1,5 +1,5 @@
 (function ($) {$(document).ready(function() {
-  if ($('body').hasClass('context-projects')) {
+  if ($('body').hasClass('context-projects') || $('body').hasClass('context-resources')) {
     $(function() {
       if ($(window).width() > 720) {
         var headfix = $('.views-exposed-widgets');
@@ -16,23 +16,3 @@
     });
   }
 });})(jQuery);
-
-
-// if ($('body').hasClass('purchases') && $('body').hasClass('new')) {
-//     $(function() {
-//       if ($(window).width() < 600) {
-//         var headfix = $('body.purchases.new header, body.purchases.new .purchase-wrapper'), pos = headfix.offset();
-//         $(window).scrollTop(85);
-//         $(window).scroll(function() {
-//           if ($(this).scrollTop() > (pos.top + 10) && $(headfix.css('position') == 'static')) {
-//             headfix.addClass('fixed');
-//             var headheight = $('header').height();
-//             $('.purchase-wrapper').css('padding-top', (headheight));
-//           } else if ($(this).scrollTop() <= (pos.top + 10) && headfix.hasClass('fixed')) {
-//             headfix.removeClass('fixed');
-//             $('.purchase-wrapper').css('padding-top', '0');
-//           };
-//         });
-//       };
-//     });
-//   };
