f8cdf5b64468c82bce69eae5a3b93860a88a5f6e
Marc Brakken <marc.brakken@gmail.com>
2013-10-27 22:50:33 -0500

    moved modifications to hybridauth template from the module to the theme

--- sites/all/modules/hybridauth/templates/hybridauth_provider_icon.tpl.php
+++ sites/all/modules/hybridauth/templates/hybridauth_provider_icon.tpl.php
@@ -1,1 +1,1 @@
-<span class="grid-8 <?php print $icon_pack_classes; ?>" title="<?php print $provider_name; ?>">Connect with <?php print $provider_name; ?></span>
+<span class="<?php print $icon_pack_classes; ?>" title="<?php print $provider_name; ?>"></span>
