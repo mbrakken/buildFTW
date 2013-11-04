cc396d9c7f9e4261d1dbaf1d96371c11c6d1bb2c
Marc Brakken <marc.brakken@gmail.com>
2013-11-03 23:12:28 -0600

    rearranged project panels template, trying to handle background image differently

--- sites/all/themes/theme_the_world/layouts/buildftw_2_10/buildftw-2-10.tpl.php
+++ sites/all/themes/theme_the_world/layouts/buildftw_2_10/buildftw-2-10.tpl.php
@@ -1,6 +1,8 @@
 <div class="panel-display omega-grid buildftw-2-10" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-12">
-    <div class="inside"><?php print $content['top']; ?></div>
+  <div id="inside-top">
+    <div class="panel-panel grid-12 top">
+      <div class="inside"><?php print $content['top']; ?></div>
+    </div>
   </div>
   <div class="panel-panel grid-2 alpha">
     <div class="inside"><?php print $content['left']; ?></div>
